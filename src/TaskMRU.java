cd328c7a5dd6a473b0dfbc37f7b6348dc0a4db62
Parker <parker.krieger@aggiemail.usu.edu>
2020-04-16 14:26:13 -0600

    Optimized MRU

--- src/TaskMRU.java
+++ src/TaskMRU.java
@@ -16,18 +16,21 @@ public class TaskMRU implements Runnable {
     @Override
     public void run() {
         ArrayList<Integer> memory = new ArrayList<>();
+        boolean[] pageTable = new boolean[this.MAX_PAGE_REFERENCE + 1];
         int pageFaults = 0;
         for (int value : sequence){
-            if (!memory.contains(value)){
+            if (!pageTable[value]){
                 pageFaults++;
                 if (memory.size() >= this.MAX_MEMORY_FRAMES + 1){
-                    memory.remove(memory.size() - 1);
+                    int removed = memory.remove(memory.size() - 1);
+                    pageTable[removed] = false;
                 }
             }
             else{
                 memory.remove((Integer) value);
             }
             memory.add(value);
+            pageTable[value] = true;
         }
         this.pageFaults[this.MAX_MEMORY_FRAMES] = pageFaults;
     }
